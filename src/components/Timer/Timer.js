d36870e81cb4b76c1793d5e4f5a2046473816fc7
HEEYOUNG SEO <hseo@HEEYOUNGs-iMac.local>
2021-08-17 01:24:01 +0900

    :recycle: refactor: 변수명 변경

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -5,13 +5,10 @@ const Timer = (props) => {
   const { country } = props
 
   const localDate = new Date()
-  const UTC = localDate.getTime() + (localDate.getTimezoneOffset() * 60 * 1000) 
-  const KR_TIME_DIFF = 9 * 60 * 60 * 1000 
-  //const utctime = Date.now()
-  //console.log(UTC)
-  //console.log(utctime)
-  const krDate = new Date(UTC + KR_TIME_DIFF)
-  console.log(krDate)
+  const LOCAL_OFFSET = localDate.getTimezoneOffset() * 60 * 1000
+  const KR_OFFSET = 9 * 60 * 60 * 1000
+  const UTC = localDate.getTime() + LOCAL_OFFSET 
+  const krDate = new Date(UTC + KR_OFFSET)
 
   const dateObj = {
     year: krDate.getFullYear(),
