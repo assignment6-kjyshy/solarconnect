7ad1f93637897f41cd565e762b7182ff3077848d
HEEYOUNG SEO <hseo@HEEYOUNGs-iMac.local>
2021-08-17 12:05:00 +0900

    :sparkle: feat: clock 추가 / state로 변경

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,26 +1,79 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { countries } from './countries'
 
 const Timer = (props) => {
   const { country } = props
+  const [time, setTime] = useState({})
+  const [clock, setClock] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
 
-  const localDate = new Date()
-  const LOCAL_OFFSET = localDate.getTimezoneOffset() * 60 * 1000
-  const KR_OFFSET = 9 * 60 * 60 * 1000
-  const UTC = localDate.getTime() + LOCAL_OFFSET 
-  const krDate = new Date(UTC + KR_OFFSET)
+  useEffect(() => {
+    const tick = () => {
+      const localDate = new Date()
+      const LOCAL_OFFSET = localDate.getTimezoneOffset() * 60 * 1000
+      const KR_OFFSET = 9 * 60 * 60 * 1000
+      const UTC = localDate.getTime() + LOCAL_OFFSET 
+      const krDate = new Date(UTC + KR_OFFSET)
+      const obj = {
+        year: krDate.getFullYear(),
+        month: krDate.getMonth() + 1,
+        date: krDate.getDate(),
+        day: krDate.getDay(),
+        hour: krDate.getHours(),
+        minute: krDate.getMinutes(),
+        second: krDate.getSeconds(),
+      }
+      setTime({
+        year: obj.year,
+        month: obj.month,
+        date: obj.date,
+        day: obj.day,
+        hour: obj.hour,
+        minute: obj.minute,
+        second: obj.second
+      })
+    }
 
-  const dateObj = {
-    year: krDate.getFullYear(),
-    month: krDate.getMonth() + 1,
-    date: krDate.getDate(),
-    day: krDate.getDay()
-  }
+    const timer = setInterval(() => tick(), 1000)
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (time.year && time.month && time.date && time.day && time.hour && time.minute && time.second) {
+      setIsLoading(false)
+  }}, [time])
+
+  useEffect(() => {
+    let h = time.hour
+    let m = time.minute;
+    let s = time.second;
+    let session;
+
+    if (h >= 12) {
+      h = h - 12;
+      session = "PM"
+    } else {
+      session = "AM"
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    setClock(`${h}:${m}:${s} ${session}`)
+  }, [time])
 
   return (
     <div>
-      <h2>{country} timer</h2>
-      <div>{country && countries[country].formatter(dateObj, countries[country])}</div>
+      <h2>{countries[country].title}</h2>
+      {!isLoading &&
+      <div> 
+      <div>{countries[country].formatter(time, countries[country])}</div>
+      <span>{clock}</span>
+      </div>
+      }
     </div>
   )
 }
--- src/components/Timer/countries.js
+++ src/components/Timer/countries.js
@@ -1,15 +1,17 @@
 export const countries = {
   KR: {
+    title: "오늘은",
     days: ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"],
-    formatter: function (dateObj, countries) {
-      return (`${dateObj.year}년 ${dateObj.month}월 ${dateObj.date}일 ${countries.days[dateObj.day]}`)
+    formatter: function (dateObj, base) {
+      return (`${dateObj.year}년 ${dateObj.month}월 ${dateObj.date}일 ${base.days[dateObj.day]}`)
     }
   },
   US: {
+    title: "Today is",
     days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
     months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    formatter: function (dateObj, countries) {
-      return (`${countries.days[dateObj.day]} ${countries.months[dateObj.month - 1]} ${dateObj.date}, ${dateObj.year}`)
+    formatter: function (dateObj, base) {
+      return (`${base.days[dateObj.day]} ${base.months[dateObj.month - 1]} ${dateObj.date}, ${dateObj.year}`)
     }
   }
 }
